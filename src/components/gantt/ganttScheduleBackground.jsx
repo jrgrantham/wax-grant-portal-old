5a1418e274e61c78fabf724cc1c7e573cd77614c
James Grantham <jamesgrantham@192.168.1.106>
2021-02-18 12:59:27 +0000

    basic tippy

--- src/components/gantt/ganttScheduleBackground.jsx
+++ src/components/gantt/ganttScheduleBackground.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
+import Tippy from "@tippy.js/react";
+import "tippy.js/dist/tippy.css";
 
 function GanttScheduleBackground() {
   const { dates } = useSelector((state) => state.project.data);
@@ -11,10 +13,16 @@ function GanttScheduleBackground() {
     "backgroundColumn columnRight",
   ];
 
+  const toolTip = <div className='tooltip'>text here</div>;
+
   const backgroundColumn = dates.map((date, i) => {
     return (
       <div key={i} className={classNames[i % 3]}>
-        {date[0]}
+        {/* <div className='month'> */}
+        <Tippy content={toolTip}>
+          <p>{date[0]}</p>
+        </Tippy>
+        {/* </div> */}
       </div>
     );
   });
@@ -30,21 +38,39 @@ const Container = styled.div`
   height: 100%;
   border-radius: 10px;
   overflow: hidden;
+  font-weight: 500;
+  color: white;
+  .month {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 30px;
+    height: 30px;
+    background-color: darkorange;
+    border-radius: 50%;
+  }
   .backgroundColumn {
     height: 100%;
     width: 40px;
     background-color: rgba(100, 100, 100, 0.1);
     border-left: 1px solid rgba(100, 100, 100, 0.1);
     border-right: 1px solid rgba(100, 100, 100, 0.1);
+    border-radius: 10px 10px 0 0;
+
     display: flex;
     justify-content: center;
+    align-items: flex-start;
     padding-top: 5px;
-    font-weight: 500;
   }
   .columnLeft {
     border-left: 1px solid white;
   }
   .columnRight {
     border-right: 1px solid white;
+  }
+
+  .tooltip {
+    background-color: black;
+    color: white;
   }
 `;
