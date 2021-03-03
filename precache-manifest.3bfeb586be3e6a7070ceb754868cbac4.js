29b6520c350330fa10aacd837a9446dfa6fcfa1f
James Grantham <jamesgrantham@192.168.1.106>
2021-03-03 18:30:34 +0000

    Updates

---
+++ precache-manifest.3bfeb586be3e6a7070ceb754868cbac4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "246480bda2985bd5c4df",
+    "url": "/wax-grant-portal/static/css/main.0e55966f.chunk.css"
+  },
+  {
+    "revision": "246480bda2985bd5c4df",
+    "url": "/wax-grant-portal/static/js/main.a7f977bb.chunk.js"
+  },
+  {
+    "revision": "e0a5981a0fd694c2072f",
+    "url": "/wax-grant-portal/static/js/runtime~main.c58daead.js"
+  },
+  {
+    "revision": "b9cf9e2b464d30cbb776",
+    "url": "/wax-grant-portal/static/css/2.60dbcbc6.chunk.css"
+  },
+  {
+    "revision": "b9cf9e2b464d30cbb776",
+    "url": "/wax-grant-portal/static/js/2.fcc384c0.chunk.js"
+  },
+  {
+    "revision": "71c9d391119af9a48d45",
+    "url": "/wax-grant-portal/static/js/3.e0e211e6.chunk.js"
+  },
+  {
+    "revision": "f23381715ba315af2502207c10215ff7",
+    "url": "/wax-grant-portal/static/media/background.f2338171.jpg"
+  },
+  {
+    "revision": "e17e058e006a91277141bc3f4442caca",
+    "url": "/wax-grant-portal/index.html"
+  }
+];
