871ebcf3693da447087153aa82348c63b946ba3b
James Grantham <jamesgrantham@192.168.1.106>
2021-04-09 16:05:43 +0100

    Updates

--- precache-manifest.f27ce3b8d626f29b6ea34b23c1aec710.js
+++ precache-manifest.f27ce3b8d626f29b6ea34b23c1aec710.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = [
   {
-    "revision": "bf7b283fc06bb39023e5",
+    "revision": "4eae734501105b527472",
     "url": "/wax-grant-portal/static/css/main.f2c15b86.chunk.css"
   },
   {
-    "revision": "bf7b283fc06bb39023e5",
-    "url": "/wax-grant-portal/static/js/main.7dc1fc06.chunk.js"
+    "revision": "4eae734501105b527472",
+    "url": "/wax-grant-portal/static/js/main.8a1dcb83.chunk.js"
   },
   {
     "revision": "faa7c3321385808faa0f",
     "url": "/wax-grant-portal/static/js/runtime~main.ced8000d.js"
   },
   {
-    "revision": "f25fa377874eeae80f02",
+    "revision": "fd393c22fb6394262eee",
     "url": "/wax-grant-portal/static/css/2.60dbcbc6.chunk.css"
   },
   {
-    "revision": "f25fa377874eeae80f02",
-    "url": "/wax-grant-portal/static/js/2.7d833c98.chunk.js"
+    "revision": "fd393c22fb6394262eee",
+    "url": "/wax-grant-portal/static/js/2.5b2b9c10.chunk.js"
   },
   {
     "revision": "4d616f84626015239d9f",
@@ -28,7 +28,7 @@ self.__precacheManifest = [
     "url": "/wax-grant-portal/static/media/background.f2338171.jpg"
   },
   {
-    "revision": "0bbb28511e51b5cac6f4bf7cfba07ff5",
+    "revision": "11c34753ff9bd96c05fe5ba72c2dcd45",
     "url": "/wax-grant-portal/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wax-grant-portal/precache-manifest.8d57eaa624ba39bf786d8cdc124e6a51.js"
+  "/wax-grant-portal/precache-manifest.f27ce3b8d626f29b6ea34b23c1aec710.js"
 );
 
 workbox.clientsClaim();
